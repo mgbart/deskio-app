33fd79245b8244271d4ab0543ee5b686fa1a59b3
Matias Bernhardt <matias.bernhardt@gmail.com>
2022-03-16 22:56:30 -0300

    basic routes added

--- frontend/main.py
+++ frontend/main.py
@@ -1,14 +1,28 @@
 from flask import Flask, render_template
+import requests
+import json
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+apiserver = "localhost:8000"
+
+def get_asset(assetid):
+    url = "http://" + apiserver + "/asset/" + assetid
+    print(url)
+    asset = requests.get(url)
+    asset_data = json.loads(asset.text)
+    return asset_data['data']
+
+
+@app.route('/checkin/<asset_id>')
+def index(asset_id):
+    asset_data = get_asset(asset_id)
     return render_template('checkin.html')
 
 @app.route('/code-check')
 def codecheck():
     return render_template('code-check.html')
+    
 
 @app.route('/code-enter')
 def codeenter():
